e406e0f9341b334c6f44061008864f9d02142b2a
Saurav Shah <thesauravs@Sauravs-MacBook-Pro.local>
2019-09-01 21:33:09 +0530

    Some more C files

--- test.c
+++ test.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include"bubbleSort.h"
 
 int main(){
-    printf("Hello World\n");
+    int n[10];
+   for (int i = 0; i < 10; i++){
+       n[i] = (rand() % 20 ) + 1;
+       printf("%d ", n[i]);
+   }
+
+    bubbleSort(n, 10);
+   
+    printf("sorted \n");
+    for(int i = 0; i < 10; i++){
+        printf("%d ", n[i]);
+    }
+   
 return 0;
 }
